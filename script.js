561311de2ebb9b7afe19497e96a32b30b09e9284
Alex HM <alex13pks@gmail.com>
2023-09-20 18:01:19 -0600

    Added Tailwind and comments section styles

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const buttonComentar = document.querySelector('#comentar');
 
 const cajita = document.querySelector('#comentarios');
 function agregarComentario() {
+  if (inputComentario.value === '') return;
   // const comentario = inputComentario.value;
   const comentario = Dompurify.sanitize(inputComentario.value);
-  cajita.innerHTML += comentario;
+  cajita.innerHTML += `
+  <article class="border-2 border-[#333] p-2 flex gap-4">
+  <div>
+  <img class="max-w-[16rem] rounded-full" src="https://picsum.photos/50" alt="imagen de perfil" class="rounded-full">
+  </div>
+  <div>
+  <h3 class="text-xl font-bold">Anonimo</h3>
+  <p>${comentario}</p>
+  </div>
+  </article>
+  `;
+  inputComentario.value = '';
 }
 
 buttonComentar.addEventListener('click', agregarComentario);
